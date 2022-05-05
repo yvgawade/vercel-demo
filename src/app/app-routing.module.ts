e287ee3dd312d6c440213a9a7114534e52387ef7
yogesh <yvgawade1@gmail.com>
2022-05-05 21:38:42 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes =  [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
 ];
 
